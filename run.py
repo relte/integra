e7d583a71ef7803cb349b739e33c027877cf9e20
relte <9462146+relte@users.noreply.github.com>
2019-09-16 21:20:11 +0200

    Provide a scheduler to run jobs periodically

--- run.py
+++ run.py
@@ -1,22 +1,34 @@
 import os
+import sys
+import schedule
+import time
 
 import bitbucket.auth
 import bitbucket.pull_requests
 import slack.broadcast
 import slack.message
 
-bitbucket.auth.prepare_token()
 
-message_builder = slack.message.PullRequestMessageBuilder()
-message_builder.with_current_date()
-counter = 0
-for repo in os.getenv('BITBUCKET_REPOSITORIES').split(';'):
-    message_builder.with_repository(repo, 'https://bitbucket.org/%s/pull-requests/' % repo)
-    pull_requests = bitbucket.pull_requests.pull_requests_to_review(repo)
-    counter += len(pull_requests)
-    for pull_request in pull_requests:
-        message_builder.with_pull_request(pull_request['title'], pull_request['href'])
-    message_builder.with_new_line()
+def main():
+    bitbucket.auth.prepare_token()
 
-if counter > 0:
-    slack.broadcast.send(message_builder.build())
+    message_builder = slack.message.PullRequestMessageBuilder()
+    message_builder.with_current_date()
+    counter = 0
+    for repo in os.getenv('BITBUCKET_REPOSITORIES').split(';'):
+        message_builder.with_repository(repo, 'https://bitbucket.org/%s/pull-requests/' % repo)
+        pull_requests = bitbucket.pull_requests.pull_requests_to_review(repo)
+        counter += len(pull_requests)
+        for pull_request in pull_requests:
+            message_builder.with_pull_request(pull_request['title'], pull_request['href'])
+        message_builder.with_new_line()
+
+    if counter > 0:
+        slack.broadcast.send(message_builder.build())
+
+
+schedule.every(int(sys.argv[1])).seconds.do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
